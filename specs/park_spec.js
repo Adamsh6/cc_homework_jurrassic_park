d918263345bf0caa05af19cd3fc05112331131d9
Adamsh6 <adam.s.houten@gmail.com>
2019-06-11 21:28:15 +0100

    wrote tests for park

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,23 +5,57 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 
   beforeEach(function () {
-
+    tRex = new Dinosaur('t-rex', 'carnivore', 50);
+    velociraptor = new Dinosaur('velociraptor', 'carnivore', 130);
+    triceratops = new Dinosaur('triceratops', 'herbivore', 40)
+    tRex2 = new Dinosaur('t-rex', 'carnivore', 60)
+    dinoArray = [tRex, velociraptor]
+    park = new Park('Triassic Retreat', 30, dinoArray)
   })
 
-  it('should have a name');
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Triassic Retreat');
+  });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function(){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 30);
+  });
 
-  it('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, dinoArray);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.addDinosaur(triceratops);
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [tRex, velociraptor, triceratops]);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.removeDinosaur(velociraptor);
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [tRex]);
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    const actual = park.mostPopularDinosaur();
+    assert.strictEqual(actual, velociraptor);
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to find all dinosaurs of a particular species', function(){
+    park.addDinosaur(tRex2);
+    const actual = park.findBySpecies('t-rex');
+    assert.deepStrictEqual(actual, [tRex, tRex2]);
+  });
 
-  it('should be able to remove all dinosaurs of a particular species');
+  it('should be able to remove all dinosaurs of a particular species', function(){
+    park.addDinosaur(tRex2);
+    park.removeBySpecies('t-rex');
+    const actual = park.dinosaurs
+    assert.deepStrictEqual(actual, [velociraptor])
+  });
 
 });
